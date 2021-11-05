3b38ae63d693f829e42ec149d0ccc9558dfb844f
Anthony Warstorm <deathpax@gmail.com>
2021-11-04 19:00:12 -0500

    Initial Commit

---
+++ InfiniteEnergyRedux/MainPatcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Harmony;
+
+// You can use this file almost as-is. Just change the marked lines below. This will be the main file of each mod that tells Harmony to load your changes.
+namespace com.deathpax.mods.GraveyardKeeper.InfiniteEnergyRedux     // Change this line to match your mod.
+{
+    public class MainPatcher
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.deathpax.mods.graveyardkeeper.infiniteenergyredux");   // Change this line to match your mod. 
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
---
+++ InfiniteEnergyRedux/PlayerComponent_Patcher.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace com.deathpax.mods.GraveyardKeeper.InfiniteEnergyRedux  // This is usually the name of your mod.
+{
+    /// <summary>
+    /// All code by Oldark and his original Infinite Energy mod. https://www.nexusmods.com/graveyardkeeper/mods/3
+    /// 
+    /// This is essentially a recompile with some references updated to support 
+    /// the new version of UnityEngine used in Graveyard Keeper.
+    /// </summary>
+    [HarmonyPatch(typeof(PlayerComponent))]  // We're patching the PlayerComponent class.
+    [HarmonyPatch("Update")]        // The PlayerComponent class's CheckEnergy method specifically.
+    internal class PlayerComponent_Update_Patch
+    {
+        public static bool infiniteEnabled = true;
+
+        [HarmonyPrefix]      // Run this after the default game's PlayerComponent Update method runs.
+        public static bool Prefix(PlayerComponent __instance)
+        {
+            if (Input.GetKey(KeyCode.L))
+            {
+                infiniteEnabled = !infiniteEnabled;
+            }
+
+            if (PlayerComponent_Update_Patch.infiniteEnabled)
+            {
+                MainGame.me.player.energy = (float)MainGame.me.save.max_energy - 1;
+            }
+
+            return true;
+        }
+    }
+}
